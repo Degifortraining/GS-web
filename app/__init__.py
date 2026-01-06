14e9fef5a27aac2685f941ba52c0bca3427162bf
Degifortraining <Degifortraining@gmail.com>
2026-01-07 00:14:53 +0800

    Checkpoint 1: initial project

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+﻿from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder="../templates",
+        static_folder="../static"
+    )
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-only-change-me")
+
+    @app.route("/")
+    def home():
+        return render_template("main/home.html")
+
+    return app
